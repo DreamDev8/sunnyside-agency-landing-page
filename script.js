99566ac416c240992bb233d07e65ed58828b6e57
Your Name <you@example.com>
2021-08-27 17:08:06 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/////////////// MOBILE MENU
+
+function onClickMenu() {
+    document.querySelector(".mobile-menu").classList.toggle("change");
+    //document.querySelector(".desktop-menu").classList.toggle("change");
+}
+
+
